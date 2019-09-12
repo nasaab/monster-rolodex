112c4477c80348928a1063badd1cc8eb3c93ac5c
Nasaab <nahmad@xcaliberinfotech.com>
2019-09-12 20:27:07 +0530

    Updates

---
+++ precache-manifest.1a7a7134abf5a7c96b2b3fdc72185123.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b1fc6de1b04ff1f3b330195e36b02e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "8eec6e0835e6a85fe667",
+    "url": "/monster-rolodex/static/css/main.a06500c9.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monster-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "8eec6e0835e6a85fe667",
+    "url": "/monster-rolodex/static/js/main.75cfdbab.chunk.js"
+  },
+  {
+    "revision": "52b3135f1aa77ef4ddba",
+    "url": "/monster-rolodex/static/js/runtime~main.b7028ca9.js"
+  }
+]);
